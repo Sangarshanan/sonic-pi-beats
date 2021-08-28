b2ea2cdcacbacac441817b6112c2529ee65afdb4
sangarshanan <sangarshanan1998@gmail.com>
2021-08-28 17:26:23 +0530

    🎼

---
+++ runaway.rb
@@ -0,0 +1,41 @@
+use_bpm 86
+
+define :piano do
+  use_synth :piano
+  5.times do
+    play :E6
+    sleep 2
+  end
+  play :E5
+  sleep 2
+  3.times do
+    play :Eb6
+    sleep 2
+  end
+  play :Eb5
+  sleep 2
+  3.times do
+    play :Db6
+    sleep 2
+  end
+  play :Db5
+  sleep 2
+  2.times do
+    play :A5
+    sleep 2
+  end
+  play :Ab5
+  sleep 2
+  play :E6
+end
+
+piano
+
+live_loop :piano_loop do
+  piano
+end
+
+live_loop :amen_break do
+  sample :loop_amen, beat_stretch: 4, cutoff: 100
+  sleep 4
+end
---
+++ stranger_tings.rb
@@ -0,0 +1,33 @@
+use_bpm 69
+
+tick = 0.25
+sound = 0.3
+live_loop :beat_1 do
+  sample :bd_haus
+  sleep 0.5
+end
+
+live_loop :beat_2 do
+  sample :drum_cymbal_pedal
+  sleep 1
+end
+
+use_synth :prophet
+live_loop :melody do
+  play :C4, amp: sound
+  sleep tick
+  play :E4, amp: sound
+  sleep tick
+  play :G4, amp: sound
+  sleep tick
+  play :B4, amp: sound
+  sleep tick
+  play :C5, amp: sound
+  sleep tick
+  play :B4, amp: sound
+  sleep tick
+  play :G4, amp: sound
+  sleep tick
+  play :E4, amp: sound
+  sleep tick
+end
