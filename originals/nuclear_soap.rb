855e1cd26e7e08b975636fda8c0416a76bd3800f
sangarshanan <sangarshanan@gmail.com>
2023-05-22 23:23:10 +0530

    soap

--- originals/nuclear_soap.rb
+++ originals/nuclear_soap.rb
@@ -53,23 +53,6 @@ live_loop :flibble do
   sleep 1
 end
 
-live_loop :bass do
-  ##| stop
-  use_synth :tech_saws
-  play :d3, attack: 0, release: 0.25
-  sleep 0.25
-  play :c3, attack: 0, release: 0.5
-  sleep 1
-  play :e3
-  sleep 0.25
-  play :f3
-  sleep 0.25
-  play :g3
-  sleep 0.25
-  play :c3, attack: 0, release: 2
-  sleep 2
-end
-
 
 live_loop :chords do
   ##| stop
