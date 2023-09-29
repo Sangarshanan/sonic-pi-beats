ec83a9c26c159d63f72b1f8a2cf720c4b42897cd
sangarshanan <sangarshanan@gmail.com>
2023-09-29 15:00:19 +0530

    let him cook

--- originals/beats/808s is the new Tanpura/bansur.rb
+++ originals/beats/808s is the new Tanpura/bansur.rb
@@ -2,7 +2,7 @@ use_bpm 100
 
 flute = "/Users/sangarshanan/Downloads/samples/Indian/flute1.wav"
 live_loop :flute   do
-  ##| stop
+  stop
   with_fx :level do
     sample flute, finish: 0.5, beat_stretch: 19, amp: 0.5
     sleep 9.6
@@ -13,7 +13,7 @@ end
 
 effect = "/Users/sangarshanan/Downloads/samples/Kaytranada/Instrument one-shot/BULLETS.wav"
 live_loop :effect do
-  ##| stop
+  stop
   with_fx :reverb do
     sleep 8.5
     sample effect, amp: 0.8, rate: 1.5, beat_stretch: 6
@@ -21,9 +21,30 @@ live_loop :effect do
   end
 end
 
-drum_loop = "/Users/sangarshanan/Downloads/samples/Lofi/Loops/lofi drum loop (1).wav"
+drum_loop1 = "/Users/sangarshanan/Downloads/samples/Lofi/Loops/lofi drum loop (1).wav"
 live_loop :drum_loop, sync: :flute do
+  stop
+  sample drum_loop1, finish: 0.5, amp: 2
+  sleep (sample_duration drum_loop1)/2
+end
+
+#### NEW LOOP ####
+
+live_loop :flute2   do
   ##| stop
-  sample drum_loop, finish: 0.5, amp: 2
-  sleep (sample_duration drum_loop)/2
+  with_fx :octaver do
+    sample flute, finish: 0.5, beat_stretch: 20, amp: 0.5
+    sleep 10
+    
+    sample flute, start: 0.5, beat_stretch: 20, amp: 0.5
+    sleep 10
+    
+  end
+end
+
+drum_loop2 = "/Users/sangarshanan/Downloads/samples/Lofi/Loops/lofi drum loop (15).wav"
+live_loop :drum_loop2, sync: :flute2 do
+  ##| stop
+  sample drum_loop2, amp: 3, beat_stretch: 10
+  sleep 10
 end
--- originals/beats/808s is the new Tanpura/sarodu.rb
+++ originals/beats/808s is the new Tanpura/sarodu.rb
@@ -2,22 +2,55 @@ use_bpm 100
 
 sarod = "/Users/sangarshanan/Downloads/samples/Indian/sarod1.wav"
 live_loop :sarod  do
-  ##| stop
+  stop
   with_fx :level do
-    3.times do
-      sample sarod, beat_stretch: 20
-      sleep 7.5
-    end
-    5.times do
+    sample sarod, beat_stretch: 20
+    sleep 20
+    2.times do
       sample sarod, start: 0.5, beat_stretch: 20
-      sleep 7.5
+      sleep 10
+    end
+    2.times do
+      sample sarod, finish: 0.5, beat_stretch: 20
+      sleep 10
     end
   end
 end
 
-drum_loop = "/Users/sangarshanan/Downloads/samples/Lofi/Loops/lofi drum loop (4).wav"
-live_loop :drum_loop, delay: 7.5 do
+guit = "/Users/sangarshanan/Downloads/samples/lofi/LofiJuice/kendrickFmin.wav"
+live_loop :guit do
   ##| stop
+  with_fx :octaver, mix: 0.5 do
+    sample guit, beat_stretch: 20, amp: 0.4
+    sleep 20
+  end
+end
+
+live_loop :sarod_half  do
+  ##| stop
+  with_fx :panslicer do
+    sleep 2.5
+    sample sarod, beat_stretch: 21, start: 0.38, finish: 0.48, amp: 0.3
+    sleep 2.5
+  end
+end
+
+base_note = (scale :c2, :minor_pentatonic, num_octaves: 1).shuffle
+
+live_loop :bass do
+  ##| stop
+  use_random_seed 3200
+  8.times do
+    n = base_note.tick
+    synth :fm, note: n, amp: 0.5, release: 0, sustain: 2.5
+    sleep 2.5
+  end
+end
+
+
+drum_loop = "/Users/sangarshanan/Downloads/samples/Lofi/Loops/lofi drum loop (4).wav"
+live_loop :drum_loop, sync: :sarod do
+  stop
   sample drum_loop, amp: 2, beat_stretch: 10
   sleep 10
 end
