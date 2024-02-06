b5a243a8995114772a9dd8fb36c473c0f5f9f236
sangarshanan <sangarshanan1998@gmail.com>
2024-02-06 20:05:29 +0530

    Code

--- originals/electronica/konnakol.rb
+++ originals/electronica/konnakol.rb
@@ -1,23 +1,32 @@
-use_bpm 180
+use_bpm 190
 
 vocal = "/Users/sangarshanan/Downloads/samples/Indian/Konnakkol1.wav"
 live_loop :vocal, sync: :bass do
   stop
   with_fx :slicer, mix: 0.3 do
-    sample vocal, beat_stretch: 180, start: 0.40, finish: 0.445
+    sample vocal, beat_stretch: 180, start: 0.40, finish: 0.445, amp: 1.5
     sleep 8
   end
 end
 
-live_loop :amen_break, sync: :bass do
+live_loop :bass2, sync: :vocal do
   stop
-  2.times do
-    3.times do
-      sample :loop_amen, amp: 1.5, beat_stretch: 4
-      sleep 4
-    end
-    sample :loop_amen_full, amp: 1.5, beat_stretch: 16, start: 0.75
-    sleep 4
+  with_fx :reverb, amp: 2 do
+    use_synth :fm
+    use_synth_defaults release: 0.2
+    play_pattern_timed [45, 43], [0.25]
+    play_pattern_timed [40, 40], [1]
+    play_pattern_timed [40], [1.75]
+    play_pattern_timed [40, 40, 40, 43, 40, 45],
+      [0.25, 0.5, 0.5, 0.5, 0.25, 1.75]
+  end
+end
+
+live_loop :all_fine, sync: :bass do
+  stop
+  with_fx :reverb do
+    sample "/Users/sangarshanan/Downloads/samples/lofi/voiceover/take a chill pill.wav", amp: 0.8
+    sleep 8
   end
 end
 
@@ -42,7 +51,6 @@ live_loop :snare, sync: :bass do
   sleep 8
 end
 
-
 live_loop :dixi1, sync: :bass do
   stop
   sample :bd_zome, rate: 3
@@ -59,10 +67,19 @@ live_loop :dixi2, sync: :bass, delay: 1 do
   sleep 8
 end
 
+live_loop :amen_break, sync: :bass do
+  stop
+  2.times do
+    3.times do
+      sample :loop_amen, amp: 1.5, beat_stretch: 4
+      sleep 4
+    end
+  end
+end
+
 mar = "/Users/sangarshanan/Downloads/samples/808_drum_kit/percussion/808-Maracas1.wav"
 live_loop :hi_hat, sync: :bass do
   stop
-  sample mar, amp: 0.4
+  sample mar, amp: 0.6
   sleep 0.5
 end
-
