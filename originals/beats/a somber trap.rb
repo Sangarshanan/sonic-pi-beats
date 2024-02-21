c7f40b55ce8e87dd751940869394774352b82414
sangarshanan <sangarshanan1998@gmail.com>
2024-02-21 12:46:50 +0530

    Loopp

---
+++ originals/beats/a somber trap.rb
@@ -0,0 +1,39 @@
+use_bpm 120
+
+pm = "/Users/sangarshanan/Downloads/samples/Melody/misc/tr-piano1.wav"
+live_loop :pm do
+  ##| stop
+  with_fx :reverb, mix: 0.5 do
+    
+    sample pm, beat_stretch: 32, amp: 3
+    sleep 32
+    
+  end
+end
+
+live_loop :house_loop, sync: :pm do
+  s = "/Users/sangarshanan/Downloads/samples/drum_loops/Trap/loop1.wav"
+  with_fx :band_eq do
+    sample s, beat_stretch: 32, amp: 5
+    sleep 32
+  end
+end
+
+live_loop :random_synth, sync: :pm do
+  use_synth :tb303
+  with_fx :slicer, amp_min: 0.20, amp_max: 0.40 do
+    25.times do
+      play choose(chord(:c3, :minor)), release: 0.4, cutoff: rrand_i(60, 130)
+      sleep 0.5
+    end
+  end
+end
+
+bass = "/Users/sangarshanan/Downloads/samples/bass/house1.wav"
+live_loop :bass, sync: :pm do
+  ##| stop
+  with_fx :reverb, mix: 0.5 do
+    sample bass, beat_stretch: 16, amp: 5
+    sleep 16
+  end
+end
--- originals/jazz something .rb
+++ originals/jazz something .rb
@@ -1,4 +1,35 @@
+set_volume! 2
+
 use_bpm 220
+
+pm = "/Users/sangarshanan/Downloads/samples/songs/pm1.wav"
+live_loop :pm_loop, sync: :chords do
+  stop
+  with_fx :reverb, mix: 0.5 do
+    
+    sample pm, amp: 2, beat_stretch: 120, finish: 0.034
+    sleep 4
+    sample pm, amp: 2, beat_stretch: 120, start: 0.034, finish: 0.08
+    sleep 4
+    sample pm, amp: 2, beat_stretch: 120, start: 0.13, finish: 0.2
+    sleep 8
+    
+    
+    use_random_seed 10  # 15, 10, 5
+    
+    # [1,2,3,4,5,6,77] # [79, 80, 81, 82, 83, 77].choose
+    # [86, 87, 88, 89]
+    
+    4.times do
+      sample pm, amp: 2, beat_stretch: 115, onset: [86, 87, 88, 89].choose
+      sleep 4
+    end
+    
+    sleep 8
+    
+    
+  end
+end
 
 live_loop :fromMidiDevice do
   use_real_time
@@ -9,10 +40,9 @@ end
 live_loop :note, sync: :chords do
   stop
   use_synth :piano
-  play 72, sustain: 8, amp: 2
+  play 72, sustain: 8, amp: 0.5
   sleep 4
 end
-
 
 live_loop :chords do
   use_synth :piano
@@ -21,6 +51,15 @@ live_loop :chords do
     sleep 8
     play [:ds3, :g3, :a3, :d4], sustain: 8, amp: 2
     sleep 8
+  end
+end
+
+live_loop :house_loop, sync: :chords do
+  stop
+  s = "/Users/sangarshanan/Downloads/samples/drum_loops/house/1.wav"
+  with_fx :band_eq do
+    sample s, beat_stretch: 32, amp: 2
+    sleep 32
   end
 end
 
@@ -33,7 +72,7 @@ swing_time = 0.1
 puts swing_time
 
 define :play_kick do
-  sample :elec_hollow_kick, amp: normal.call
+  sample :elec_hollow_kick, amp: 2
   play :A1, amp: normal.call
 end
 
@@ -41,7 +80,11 @@ define :play_charles do
   sample :drum_cymbal_pedal, attack:0.02, amp:normal.call, rate:1.1
 end
 
+kick = "/Users/sangarshanan/Downloads/samples/808_drum_kit/kicks/808-Kicks02.wav"
+
 live_loop :drums do
+  ##| stop
+  
   # Two ways of modelling beats - with 0s and 1s OR with indexes
   [1, 0, 0, 0, 1, 0, 0, 0].each.with_index(1) do |kick, index|
     play_kick if kick == 1 and one_in(3)
@@ -78,3 +121,14 @@ live_loop :drums do
     end
   end
 end
+
+mar = "/Users/sangarshanan/Downloads/samples/808_drum_kit/percussion/808-Maracas1.wav"
+
+live_loop :hihat, sync: :drums do
+  ##| stop
+  with_fx :reverb, mix: 0 do
+    sample mar, amp: rrand(1.9, 2.4), rate: rrand(1.9, 2.1), cutoff: rrand(80, 120), pan: rrand(-0.25, 0.25)
+  end
+  sleep 1
+end
+
