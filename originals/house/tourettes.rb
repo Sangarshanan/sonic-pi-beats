c631568d3b80e036366cc7bc56162b3c7b1f70ab
sangarshanan <sangarshanan1998@gmail.com>
2024-05-29 16:38:36 +0530

    stop

---
+++ originals/house/tourettes.rb
@@ -0,0 +1,75 @@
+use_bpm 150
+
+
+live_loop :flibble do
+  ##| stop
+  with_fx :lpf do
+    sample :bd_haus, rate: 1, amp: 1
+    ##| sample :bass_thick_c, amp: 0.5, rate: 0.5
+    sleep 0.5
+  end
+end
+
+
+vocal = "/Users/sangarshanan/Downloads/samples/drum_loops/dubstep/drop1.wav"
+live_loop :vocal do
+  stop
+  with_fx :echo, mix: 0.3, amp: 2 do
+    
+    ##| sample vocal, beat_stretch: 50
+    ##| sleep 50
+    
+    2.times do
+      sample vocal, beat_stretch: 50, start: 0.917
+      sleep 4
+    end
+    
+    ##| sleep 1
+    
+  end
+  
+end
+
+vocal = "/Users/sangarshanan/Downloads/samples/vocal/vine1.wav"
+
+live_loop :vocal_dandb, sync: :flibble do
+  ##| stop
+  
+  ##| 3.times do
+  ##|   with_fx :ping_pong do
+  
+  ##|     sample vocal, beat_stretch: 14, amp: 2, finish: 0.25
+  ##|     sleep 4
+  
+  ##|     sample vocal, beat_stretch: 15, amp: 2, start: 0.34, finish: 0.5
+  ##|     sleep 4
+  
+  ##|   end
+  ##| end
+  
+  
+  2.times do
+    sample vocal, beat_stretch: 15, amp: 3, start: 0.55, finish: 0.86
+    sleep 4
+    
+    sample vocal, beat_stretch: 15, amp: 3, start: 0.86
+    sleep 2
+    
+    with_fx :ixi_techno do
+      sample vocal, beat_stretch: 15, amp: 3, start: 0.86
+      sleep 2
+    end
+    
+  end
+  
+end
+
+
+live_loop :techno, sync: :vocal_dandb do
+  ##| stop
+  s = "/Users/sangarshanan/Downloads/samples/drum_loops/techno/2.wav"
+  with_fx :band_eq do
+    sample s, beat_stretch: 32, amp: 3
+    sleep 32
+  end
+end
